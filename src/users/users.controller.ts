375de2f8e5a63b43b0dfc11e97951d151924037b
Fufa Wakgari <fufawakagari174@gmail.com>
2026-02-20 11:31:05 +0300

    saving user instance to the database

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,12 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/user.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+    constructor(private usersService: UsersService){}
     @Post('/signup')
     createUser(@Body() createUserDto:CreateUserDto){
-        console.log(createUserDto);
-
+        return this.usersService.create(createUserDto.email, createUserDto.password);
     }
 }
