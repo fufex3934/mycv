c1ae56fbbba4cbe0ced9ee33b257598a16ea1d04
Fufa Wakgari <fufawakagari174@gmail.com>
2026-02-20 14:26:49 +0300

    user crud api

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Delete, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/user.dto';
 import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('auth')
 export class UsersController {
@@ -24,6 +25,11 @@ export class UsersController {
     @Delete('/:id')
     removeUser(@Param('id') id:string){
         return this.usersService.remove(parseInt(id));
+    }
+
+    @Patch('/:id')
+    updateUser(@Param('id') id:string, @Body() body:UpdateUserDto){
+        return this.usersService.update(parseInt(id), body);
     }
 
 }
